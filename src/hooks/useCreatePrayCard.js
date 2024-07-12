94d759f1ff9fff53bd95b266e11cf3a5683d6a5d
Junbeom Park <ephesi4ns623@gmail.com>
2024-07-12 14:46:36 +0900

    chore: 주석 제거

--- src/hooks/useCreatePrayCard.js
+++ src/hooks/useCreatePrayCard.js
@@ -10,7 +10,6 @@ const useCreatePrayCard = (member) => {
     if (prayerText.trim() === "") {
       alert("기도제목을 작성해주셔야 그룹원들을 위해 기도할 수 있어요");
     } else {
-      // TODO: 저장하면 DB에 넣어줘야함!
       const data = insertPrayCards(member.group_id, prayerText);
       window.location.reload();
     }

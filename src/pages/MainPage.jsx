a8c5f0a04bc5b6c76f445272cbca94829dedcaaf
mmyeong <82504981+myeongjunkim@users.noreply.github.com>
2024-07-16 18:23:22 +0900

    feat: 이미지용 캐러셀 생성 및 적용 (#25)
    * feat: 이미지용 캐러셀 생성
    
    * chore: 메인 페이지, 그룹생성 페이지 UI 수정

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -3,6 +3,11 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { Auth } from "@supabase/auth-ui-react";
 import { supabase } from "../supaClient";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/ImgCarousel";
 import useAuth from "../hooks/useAuth";
 
 const MainPage = () => {
@@ -17,8 +22,37 @@ const MainPage = () => {
   const from = location.state?.from?.pathname || "/group";
   const redirectUrl = `${import.meta.env.VITE_BASE_URL}${from}`;
   return (
-    <div>
-      <h3 className="text-center text-5xl mt-10">안녕하세요, PrayU입니다.</h3>
+    <div className="flex flex-col text-center items-center gap-2">
+      <div className="flex flex-col gap-2 mt-20 ">
+        <div className="text-lg font-bold">
+          우리만의 기도제목 기록공간, PrayU
+        </div>
+        <div className="text-sm text-gray-500">
+          매일 기도하고 매일 반응하는 우리 공동체
+        </div>
+      </div>
+
+      <Carousel opts={{ align: "start" }} className="w-full aspect-square">
+        <CarouselContent className="flex gap-4 full aspect-square">
+          {/* 파란색 배경*/}
+          <CarouselItem className="w-full aspect-square p-6">
+            <div className="flex justify-center items-center w-full aspect-square bg-gray-300">
+              1
+            </div>
+          </CarouselItem>
+          <CarouselItem className="w-full aspect-square p-6">
+            <div className="flex justify-center items-center w-full aspect-square bg-gray-300">
+              2
+            </div>
+          </CarouselItem>
+          <CarouselItem className="w-full aspect-square  p-6">
+            <div className="flex justify-center items-center w-full aspect-square bg-gray-300">
+              3
+            </div>
+          </CarouselItem>
+        </CarouselContent>
+      </Carousel>
+
       <Auth
         redirectTo={redirectUrl}
         supabaseClient={supabase}
